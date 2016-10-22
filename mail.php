6e7e1fbb6b2062689116822ba3fa10df0d13fc51
Daniel Snitkovskiy <Daniel@D-173-250-180-111.dhcp4.washington.edu>
2016-10-22 15:51:39 -0700

    Syntax error in mail.php

--- mail.php
+++ mail.php
@@ -10,9 +10,9 @@
 
   if($_POST['submit']){
     if(mail ($to, $subject, $body, $from)){
-      echo "<p>Your message has been sent!</p>"
+      echo "<p>Your message has been sent!</p>";
     } else{
-      echo "<p><Something went wrong, go back and try again./p>"
+      echo "<p><Something went wrong, go back and try again.</p>";
     }
   }
 
