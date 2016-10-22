79ce406edb2fd04a32e1454d8a13cff4d916ff23
Daniel Snitkovskiy <Daniel@D-173-250-180-111.dhcp4.washington.edu>
2016-10-22 16:13:05 -0700

    Simpilfied mail.php

---
+++ mail.js
@@ -0,0 +1,3 @@
+$(".button.submit"){
+  console.log("Submitted!");
+}
--- mail.php
+++ mail.php
@@ -1,4 +1,5 @@
 <?php
+
   $name = $_POST['name'];
   $email = $_POST['email'];
   $message = $_POST['message'];
@@ -8,12 +9,7 @@
   $to = "snitkdan@uw.edu";
   $subject = "Website Contact Form";
 
-  if($_POST['submit']){
-    if(mail ($to, $subject, $body, $from)){
-      echo "<p>Your message has been sent!</p>";
-    } else{
-      echo "<p><Something went wrong, go back and try again.</p>";
-    }
-  }
+  mail ($to, $subject, $body, $from));
+  echo "<p>Your message has been sent!</p>";
 
- ?>
+?>
